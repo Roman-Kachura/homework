a5e7488634bc6b85042d33f99ed4d232ac2625e7
Roman-Kachura <Roman-Kachura@users.noreply.github.com>
2021-09-16 09:55:17 +0200

    Updates

---
+++ precache-manifest.54c4d3732611f6f8440c23c7ca41d45b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d9a37677c2c07a54d741f5e2e78458",
+    "url": "/homework1/index.html"
+  },
+  {
+    "revision": "b07d357cee857c0ef640",
+    "url": "/homework1/static/css/main.a2a68b8b.chunk.css"
+  },
+  {
+    "revision": "043e48ac280e38c3fada",
+    "url": "/homework1/static/js/2.2fea7f0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homework1/static/js/2.2fea7f0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b07d357cee857c0ef640",
+    "url": "/homework1/static/js/main.13d42be9.chunk.js"
+  },
+  {
+    "revision": "c28766352f522ae92250",
+    "url": "/homework1/static/js/runtime-main.bfbdd52a.js"
+  }
+]);
